02bc4376587c7b93eb068adb409dc11c342c6371
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2023-01-03 18:50:52 +0200

    Fix

--- TemplateUI/TemplateUI/Program.cs
+++ TemplateUI/TemplateUI/Program.cs
@@ -1,22 +1,16 @@
-using Blazored.Toast;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TemplateUI;
 using TemplateUI.Extensions;
 
 namespace TemplateUI
 {
     public class Program
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.AddRootComponents();
-
-            builder.AddClientServices();
-
-            await builder.Build().RunAsync();
-        }
+        public static async Task Main(string[] args) =>
+            await WebAssemblyHostBuilder
+                .CreateDefault(args)
+                .AddRootComponents()
+                .AddClientServices()
+                .Build()
+                .RunAsync();
     }
 }
